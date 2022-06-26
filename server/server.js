32c12d0b1545c85224ee2c40617583be11383da3
sam-levin <sam.ky.levin@gmail.com>
2022-06-26 15:18:56 -0700

    adding concurrent functionality and react

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express')
 const { authMiddleware } = require('./utils/auth');
@@ -15,6 +17,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
